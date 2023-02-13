fabe82dcdd883c1c99ba231c3125f6f45a9a89f6
pavelkarpovich2015 <pavelkarpovich2015@gmail.com>
2023-02-13 17:50:59 +0300

    Add project files.

---
+++ TurboCollection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TurboCollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
